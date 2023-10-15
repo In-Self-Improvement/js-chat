822cd6edb20382080ee1de140517e519414f4d4c
Bong <borobong230@gmail.com>
2023-10-15 10:57:20 +0900

    feat: Install nodemon(for refresh server), add socketio, http in app.js, test chat.js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // 서버 구동용!
 
 const express = require("express");
+const http = require("http");
 const app = express();
 const path = require("path");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "src")));
-
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("연결 완료");
+});
+
+server.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
 });
