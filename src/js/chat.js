d4d85f9f5f237e0036e1df813a8054c88fea6c9e
Bong <borobong230@gmail.com>
2023-12-10 19:43:17 +0900

    feat: chat.js 오류 수정

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,16 +1,22 @@
-// 클라이언트 코드
 const socket = io.connect("http://localhost:5001");
 
 const chatForm = document.querySelector(".chat-form");
 const chatInput = document.querySelector(".chat-input");
+const sendButton = document.querySelector(".chat-button");
 
-chatForm.addEventListener("submit", function (e) {
+sendButton.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("test", test);
 
   const message = chatInput.value;
   chatInput.value = "";
 
-  // 서버로 메시지를 전송
   socket.emit("chatMessage", message);
+});
+
+// 메시지를 받았을 때의 처리
+socket.on("chatMessage", function (message) {
+  const chatList = document.querySelector(".chat-list");
+  const newMessage = document.createElement("div");
+  newMessage.textContent = message;
+  chatList.appendChild(newMessage);
 });
