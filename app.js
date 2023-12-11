aa43882ebbf53be04d5f92742ba9e64978c482aa
Bong <borobong230@gmail.com>
2023-12-11 18:54:09 +0900

    feat: 상대방 메세지, 내 메세지 분리

--- app.js
+++ app.js
@@ -11,12 +11,29 @@ app.use(express.static(path.join(__dirname, "src")));
 const PORT = process.env.PORT || 5001;
 
 io.on("connection", (socket) => {
-  socket.on("chatMessage", (message) => {
+  socket.on("myMessage", (message) => {
     console.log("Message received: ", message);
 
-    io.emit("chatMessage", message);
+    io.emit("myMessage", message);
   });
 });
+
+io.on("connection", (socket) => {
+  socket.on("yourMessage", (message) => {
+    console.log("Message received: ", message);
+
+    io.emit("yourMessage", message);
+  });
+});
+
+// io.on("connection", (socket) => {
+//   socket.on("myMessage", (message) => {
+//     console.log("Message received: ", message);
+
+//     // 다른 사용자에게 메시지를 보냅니다.
+//     socket.broadcast.emit("yourMessage", message);
+//   });
+// });
 
 server.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
--- src/js/chat.js
+++ src/js/chat.js
@@ -3,20 +3,25 @@ const socket = io.connect("http://localhost:5001");
 const chatForm = document.querySelector(".chat-form");
 const chatInput = document.querySelector(".chat-input");
 const sendButton = document.querySelector(".chat-button");
-
+const chatList = document.querySelector(".chat-list");
 sendButton.addEventListener("click", function (e) {
   e.preventDefault();
 
   const message = chatInput.value;
   chatInput.value = "";
 
-  socket.emit("chatMessage", message);
-});
-
-// 메시지를 받았을 때의 처리
-socket.on("chatMessage", function (message) {
-  const chatList = document.querySelector(".chat-list");
   const newMessage = document.createElement("div");
   newMessage.textContent = message;
+  newMessage.classList.add("user-message");
+
+  chatList.appendChild(newMessage);
+  socket.emit("myMessage", message);
+});
+
+socket.on("yourMessage", function (message) {
+  const newMessage = document.createElement("div");
+  newMessage.textContent = message;
+  newMessage.classList.add("other-message");
+
   chatList.appendChild(newMessage);
 });
